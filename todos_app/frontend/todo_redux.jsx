8ce6509fe03a9e78fac50d904a7d5e8763401df5
Julian Napolitan <julian.napolitan@gmail.com>
2018-12-03 20:35:04 -0800

    Remove excess files and update selectors

--- todos_app/frontend/todo_redux.jsx
+++ todos_app/frontend/todo_redux.jsx
@@ -4,11 +4,13 @@ import configureStore from '../store/store';
 import Root from './components/root';
 import { receiveTodos } from '../actions/todo_actions';
 import { receiveTodo } from '../actions/todo_actions';
+import { allTodos } from '../reducers/selectors';
 
 const store = configureStore;
 window.store = store;
 window.receiveTodos = receiveTodos;
 window.receiveTodo = receiveTodo;
+window.allTodos = allTodos;
 
 document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(
